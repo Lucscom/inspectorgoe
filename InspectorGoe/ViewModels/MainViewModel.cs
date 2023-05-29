fd7384267b15c404010f57bec740933375fc2780
Lucscom <mainitz@live.de>
2023-05-29 12:20:29 +0200

    Proof of Concept

---
+++ InspectorGoe/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using GameComponents;
+
+namespace InspectorGoe.ViewModels;
+public partial class MainViewModel : ObservableObject
+{
+    private Controller controller = Controller.GetInstance();
+    private List<Player> detectives;
+
+    public MainViewModel()
+    {
+        controller.Initialize(4);
+
+        detectives = new List<Player>(controller.Detectives);
+
+        var X = detectives.First();
+        X.ScooterTicket = 3;
+        X.BikeTicket = 3;
+        X.BusTicket = 3;
+    }
+
+    public List<Player> DetectivesCollection
+    {
+        get { return detectives; }
+        set { detectives = value; }
+    }
+
+
+
+
+}
---
+++ InspectorGoe/ViewModels/StudentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectorGoe.ViewModels
+{
+    public class StudentViewModel
+    {
+
+        private List<Students> studentsCollection = new List<Students>();
+
+        public StudentViewModel()
+        {
+            studentsCollection.Add(new Students("Marie", "White", "+1-809-554-6055"));
+            studentsCollection.Add(new Students("Paola", "Pullman", "+1-809-506-5655"));
+            studentsCollection.Add(new Students("Joseph", "McDonalds", "+1-809-684-4876"));
+            studentsCollection.Add(new Students("Max", "Gangbanger", "+125234-5655"));
+            studentsCollection.Add(new Students("Moritz", "Bumsluemmel", "+23455532-4876"));
+        }
+
+        public List<Students> StudentsCollection
+        {
+            get { return studentsCollection; }
+            set { studentsCollection = value; }
+        }
+    }
+}
