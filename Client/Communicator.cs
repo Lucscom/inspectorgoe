39c1991bc8654376ed2b67e3317de2932186b23d
Philip Hufeland <philip.hufeland@stud.hawk.de>
2023-06-12 15:39:38 +0200

    Added signalR client for testing

--- Client/Communicator.cs
+++ Client/Communicator.cs
@@ -6,6 +6,9 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using GameComponents.Model;
 using GameComponents;
+using System.Net.WebSockets;
+using System.Text;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace InspectorGoe
 {
@@ -61,7 +64,7 @@ namespace InspectorGoe
 
         public async static Task Main()
         {
-            await RunAsync();
+            await RunAsyncSignalR();
         }
 
         static async Task RunAsync()
@@ -103,6 +106,64 @@ namespace InspectorGoe
             }
 
             Console.ReadLine();
+        }
+
+        static async Task RunAsyncSocket()
+        {
+
+            try
+            {
+
+                using (var ws = new ClientWebSocket())
+                {
+                    await ws.ConnectAsync(new Uri("wss://localhost:5000/ws"), CancellationToken.None);
+                    var buffer = new byte[256];
+                    while (ws.State == WebSocketState.Open)
+                    {
+                        var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+                        if (result.MessageType == WebSocketMessageType.Close)
+                        {
+                            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+                        }
+                        else
+                        {
+                            Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, result.Count));
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        static async Task RunAsyncSignalR()
+        {
+            HubConnection connection;
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5000/gameHub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine($"{message}");
+            });
+
+            connection.On<GameState>("ReceiveGameState", (gameState) =>
+            {
+                Console.WriteLine($"Runde: {gameState.Round}");
+            });
+
+            await connection.StartAsync();
+
+
+
+            while (true) { }
         }
     }
 }
