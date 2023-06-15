dc333f0625a67a83bba4d46ee4be008cb8f99904
Henri <Henri.reumschuessel@haw-hamburg.de>
2023-06-15 19:36:30 +0200

    communicator class finished

--- Client/Communicator.cs
+++ Client/Communicator.cs
@@ -15,29 +15,56 @@ namespace InspectorGoe
 
     public class Communicator
     {
-        private HttpClient _client; 
+        /// <summary>
+        /// Http Client manages connection
+        /// </summary>
+        private HttpClient _client;
+        /// <summary>
+        /// Bearer Token for authorization on the server
+        /// </summary>
+        private String _token;
+        /// <summary>
+        /// Server URL
+        /// </summary>
+        private String _url;
 
+        /// <summary>
+        /// Constructor for the communicator without url
+        /// </summary>
         public Communicator()
         {
             _client = new HttpClient();
 
         }
 
+        /// <summary>
+        /// Initialize the Communicator with the Server URL
+        /// </summary>
+        /// <param name="url">Server URL</param>
         public void initClient(String url)
         {
+            _url = url;
             _client.BaseAddress = new Uri(url);
             _client.DefaultRequestHeaders.Accept.Clear();
             _client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
         }
-        //todo: private token...
-        public void setToken(String token)
+
+        /// <summary>
+        /// Set the Bearer Token for Authorization and configure the HTTP Header
+        /// </summary>
+        private void setToken()
         {
             _client.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("Bearer", token);
+                new AuthenticationHeaderValue("Bearer", _token);
         }
 
+        /// <summary>
+        /// Create(Http Post) a new Player (Register)
+        /// </summary>
+        /// <param name="player">Player Object</param>
+        /// <returns>Http Status Code</returns>
         public async Task<HttpStatusCode> CreatePlayerAsync(Player player)
         {
             HttpResponseMessage response = await _client.PostAsJsonAsync(
@@ -46,17 +73,30 @@ namespace InspectorGoe
             return response.StatusCode;
         }
 
-        public async Task<String> Login(Player player)
+        /// <summary>
+        /// Login to an existing Player to get the authorization Token
+        /// (Http Post)
+        /// </summary>
+        /// <param name="player">Player Object</param>
+        /// <returns>Http Status Code</returns>
+        public async Task<HttpStatusCode> Login(Player player)
         {
             HttpResponseMessage response = await _client.PostAsJsonAsync(
                 "api/Player/login", player);
             response.EnsureSuccessStatusCode();
             String tokenJson = await response.Content.ReadAsStringAsync();
-            String token = System.Text.Json.JsonSerializer.Deserialize<Token>(
+            _token = System.Text.Json.JsonSerializer.Deserialize<Token>(
                 tokenJson).token;
-            return token;
+            setToken();
+            return response.StatusCode;
         }
 
+        /// <summary>
+        /// Make a move with a destination POI and a ticket type
+        /// (Http Put)
+        /// </summary>
+        /// <param name="move">Move Player Object</param>
+        /// <returns>Http Status Code</returns>
         public async Task<HttpStatusCode> MovePlayerAsync(MovePlayerDto move)
         {
             HttpResponseMessage response = await _client.PutAsJsonAsync(
@@ -65,23 +105,13 @@ namespace InspectorGoe
             return response.StatusCode;
         }
 
-        public async Task<Player> GetPlayer()
-        {
-            HttpResponseMessage response = await _client.GetAsync("api/Player");
-            response.EnsureSuccessStatusCode();
-            String playerJson = await response.Content.ReadAsStringAsync();
-            Player player = System.Text.Json.JsonSerializer.Deserialize<Player>(
-                playerJson);
-            return player;
-        }
-
-        public async Task RunAsyncSignalR(String token)
+        public async Task RunAsyncSignalR()
         {
             HubConnection connection;
             connection = new HubConnectionBuilder()
-                .WithUrl("https://localhost:5000/gameHub", options =>
+                .WithUrl(_url, options =>
                 {
-                    options.AccessTokenProvider = () => Task.FromResult(token);
+                    options.AccessTokenProvider = () => Task.FromResult(_token);
                 })
                 .WithAutomaticReconnect()
                 .Build();
