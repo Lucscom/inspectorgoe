664ec8da30105eb34d28d24c7f30a15fafa97773
Philip Hufeland <philip.hufeland@stud.hawk.de>
2023-06-16 20:22:59 +0200

    Added test class

---
+++ TestProject/ValidatorTests.cs
@@ -0,0 +1,117 @@
+﻿using GameComponents.Model;
+using Newtonsoft.Json;
+using System.Numerics;
+using System.IO;
+using GameComponents;
+
+namespace TestProject
+{
+    public class ValidatorTests
+    {
+        GameState GameState;
+
+        public ValidatorTests() 
+        {
+
+        }
+
+        //TODO: Write tests
+        [Fact]
+        public void AllValidMoves()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Initializes points of interest from game config
+        /// </summary>
+        /*private void InitPois()
+        {
+            //read POIs from JSON file Testmap in project folder 
+            string jsonContent = new StreamReader(FileSystem.OpenAppPackageFileAsync("Testmap.json").GetAwaiter().GetResult()).ReadToEnd();
+            dynamic json = JsonConvert.DeserializeObject(jsonContent);  //read JSON to dynamic variable
+
+            // generate POIs 
+            int numPoi = 0;     //number of POIs in JSON-File
+            foreach (var Nodes in json.Nodes)
+            {
+                GameState.PointsOfInterest.Add(new PointOfInterest((int)Nodes.Number, (string)Nodes.Name, new Vector2((float)Nodes.Location_x, (float)Nodes.Location_y)));
+                ++numPoi;
+            }
+
+            // generate Connections 
+            int numConnect = 0;     //total number of Connections 
+            var noSuccess = 0;      //  =1 in default case
+            foreach (var Connections in json.Connections)
+            {
+                switch ((int)Connections.type) //defines number and type of connections between POIs
+                {
+                    // !!! Diese Logik stimmt bisher nur, wenn die POIs in der Reihenfolge ihrer Laufnummer initialisiert werden, da No in _gameState.PointsOfInterest[No] nur die Position in der Liste repräsentiert, nicht die Laufnummer!
+                    case 1:
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bus);
+                        ++numConnect;
+                        break;
+                    case 2:
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bike);
+                        ++numConnect;
+                        break;
+                    case 3:
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Scooter);
+                        ++numConnect;
+                        break;
+                    case 12:
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bus);
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bike);
+                        numConnect = numConnect + 2;
+                        break;
+                    case 13:
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bus);
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Scooter);
+                        numConnect = numConnect + 2;
+                        break;
+                    case 23:
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bus);
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Scooter);
+                        numConnect = numConnect + 2;
+                        break;
+                    case 123:
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bus);
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Bike);
+                        ConnectPois(GameState.PointsOfInterest[(int)Connections.sourceNo - 1], GameState.PointsOfInterest[(int)Connections.targetNo - 1], TicketTypeEnum.Scooter);
+                        numConnect = numConnect + 3;
+                        break;
+                    default:
+                        noSuccess = 1;
+                        break;
+                }
+            }
+
+        }*/
+
+        /// <summary>
+        /// Creates the connection between the points of interest
+        /// </summary>
+        /// <param name="pointOfInterest1">First point of connection</param>
+        /// <param name="pointOfInterest2">Second point of connection</param>
+        /// <param name="ticketType">Type of connection</param>
+       /* private void ConnectPois(PointOfInterest pointOfInterest1, PointOfInterest pointOfInterest2, TicketTypeEnum ticketType)
+        {
+            switch (ticketType)
+            {
+                case TicketTypeEnum.Bus:
+                    pointOfInterest1.ConnectionBus.Add(pointOfInterest2);
+                    pointOfInterest2.ConnectionBus.Add(pointOfInterest1);
+                    break;
+                case TicketTypeEnum.Bike:
+                    pointOfInterest1.ConnectionBike.Add(pointOfInterest2);
+                    pointOfInterest2.ConnectionBike.Add(pointOfInterest1);
+                    break;
+                case TicketTypeEnum.Scooter:
+                    pointOfInterest1.ConnectionScooter.Add(pointOfInterest2);
+                    pointOfInterest2.ConnectionScooter.Add(pointOfInterest1);
+                    break;
+            }
+        }*/
+    }
+}
