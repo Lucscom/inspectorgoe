f18ce1cdc9905c8380052088f1b8e609b5b0259c
06heol <ole-98-@gmx.de>
2023-06-22 14:49:30 +0200

    NoTicketNoMovement changed (added StartGame and make it runable) TicketMovement added: works for test connection

--- TestProject/ControllerTests.cs
+++ TestProject/ControllerTests.cs
@@ -25,70 +25,52 @@ namespace TestProject
             Initializer.InitPois(TestGameState);        // generate POIs and Connections
 
             TestGameState.Detectives.Add(new Player("TestPlayer", "TestPassword"));   // generate TestPlayer
+            TestGameState.MisterX = (new Player("TestX", "TestPassword"));   // generate Mister X for tests (needed to Start game)
             TestGameState.Detectives[0].Position = TestGameState.PointsOfInterest.First(); // assign Startposition for TestPlayer
             TestGameState.Detectives[0].BusTicket = 0;      // assign bus tickets
             TestGameState.Detectives[0].BikeTicket = 0;     // assign bike tickets
             TestGameState.Detectives[0].ScooterTicket = 0;  // assign scooter tickets
 
-            // zufälllige Zielposition für den Zug festlegen
-            //PointOfInterest newPos = TestGameState.PointsOfInterest.First();
-
-            // Zielposition wählen, zu der eine Verbindung mit Bike existiert
-            PointOfInterest newPos = TestGameState.Detectives[0].Position.ConnectionBike[Random.Shared.Next(0, TestGameState.Detectives[0].Position.ConnectionBike.Count - 1)];
-
-            // testen ob Zielposition ungleich aktueller position  //wird aktuell nicht mehr benötigt, da die gleich Position ausgeschlossen 
-            /*while (TestGameState.Detectives[0].Position == newPos)
-            {
-                newPos = TestGameState.PointsOfInterest[Random.Shared.Next(0, TestGameState.PointsOfInterest.Count - 1)];
-            }*/
-
-            // testen der GameController MovePlayer method
+            // testen der GameController MovePlayer method: sollte den move nicht ausführen, da keine Scooter Tickets
             GameController TestGameController = new GameController(TestGameState);      // generate Gamecontroller
-            TestGameController.AddPlayer(TestGameState.Detectives[0]);      // add Player to GameController
+           
+            TestGameController.StartGame();     // Start Game is requirement for MovePlayer
+            TestGameController.GameState.Detectives[0].Position = TestGameController.GameState.PointsOfInterest.First(); // assign Startposition for TestPlayer
 
-            TestGameController.MovePlayer(TestGameState.Detectives[0], newPos, TicketTypeEnum.Scooter);
+            // zufällige Zielposition wählen, zu der eine Verbindung mit Scooter existiert
+            PointOfInterest newPos = TestGameState.Detectives[0].Position.ConnectionScooter[Random.Shared.Next(0, TestGameState.Detectives[0].Position.ConnectionScooter.Count - 1)];
+
+            TestGameController.MovePlayer(TestGameState.Detectives[0], newPos, TicketTypeEnum.Scooter);     // execute MovePlayer
             
-            Assert.NotEqual(TestGameState.MisterX.Position, newPos);
+            Assert.NotEqual(TestGameState.Detectives[0].Position, newPos);
 
         }
         /// <summary>
-        /// testen ob der controller einen validen Zug zulässt
+        /// tests if controller executes a valid move
         /// </summary>
         [Fact]
         public void TicketMovement()
         {
-        /*
-            var controller = Controller.GetInstance();
-            controller.Initialize(2);
+            GameState TestGameState = new GameState();      // GameState constructor
+            Initializer.InitPois(TestGameState);        // generate POIs and Connections
 
-            var X = controller.MisterX;
+            TestGameState.Detectives.Add(new Player("TestPlayer", "TestPassword"));   // generate TestPlayer
+            TestGameState.MisterX = (new Player("TestX", "TestPassword"));   // generate Mister X for tests (needed to Start game)
+            TestGameState.Detectives[0].BusTicket = 5;      // assign bus tickets
+            TestGameState.Detectives[0].BikeTicket = 5;     // assign bike tickets
+            TestGameState.Detectives[0].ScooterTicket = 5;  // assign scooter tickets
 
-            X.ScooterTicket = 5;
-            X.BikeTicket = 5;
-            X.BusTicket = 5;
+            // testen der GameController MovePlayer method: sollte den move ausführen, da Scooter Tickets vorhanden
+            GameController TestGameController = new GameController(TestGameState);      // generate Gamecontroller
+                        
+            TestGameController.StartGame();     // Start Game is requirement for MovePlayer
+            TestGameController.GameState.Detectives[0].Position = TestGameController.GameState.PointsOfInterest.First(); // assign Startposition for TestPlayer
+            // zufällige Zielposition wählen, zu der eine Verbindung mit Scooter existiert
+            PointOfInterest newPos = TestGameController.GameState.Detectives[0].Position.ConnectionScooter[Random.Shared.Next(0, TestGameController.GameState.Detectives[0].Position.ConnectionScooter.Count - 1)];
 
-            // Zielposition wird basierend auf den existierenden verbindungen des POI ausgewählt um einen validen zug zu garantieren
-            // Es wird nicht garantiert, dass ein anderer Spieler auf diesem POI steht!
-            var newPos = X.Position;
+            TestGameController.MovePlayer(TestGameController.GameState.Detectives[0], newPos, TicketTypeEnum.Scooter);     // execute MovePlayer
 
-            if (X.Position.ConnectionBus.Count != 0)                    // Busverbindung am Start-POI existiert
-            {
-                newPos = X.Position.ConnectionBus.First();                  // Neue Position ist erste aus der Liste der Busverbindungen
-                controller.MovePlayer(X, newPos, TicketTypeEnum.Bus);       // übergeben des spieler, neuen position und fortbewegungsmittel an den controller
-            }
-            else if (X.Position.ConnectionBike.Count != 0)              //Bikeverbindung am Start-POI existiert
-            {
-                newPos = X.Position.ConnectionBike.First();                 // Neue Position ist erste aus der Liste der Bikeverbindungen
-                controller.MovePlayer(X, newPos, TicketTypeEnum.Bike);      // übergeben des spieler, neuen position und fortbewegungsmittel an den controller
-            }
-            else if (X.Position.ConnectionScooter.Count != 0)           //Scooterverbindung am Start-POI existiert
-            {
-                newPos = X.Position.ConnectionScooter.First();              // Neue Position ist erste aus der Liste der Scooterverbindungen
-                controller.MovePlayer(X, newPos, TicketTypeEnum.Scooter);   // übergeben des spieler, neuen position und fortbewegungsmittel an den controller
-            }
-                             
-            Assert.Equal(X.Position, newPos);      // Klappt nicht immer (aber häufig), wenn durch Zufall ein Detective auf dem Feld steht.
-        */
+            Assert.Equal(TestGameController.GameState.Detectives[0].Position, newPos);
         }
 
         /// <summary>
