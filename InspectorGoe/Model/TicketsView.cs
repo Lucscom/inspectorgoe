228d5b05119cce0bf5e394c743a81284852885e3
philippkober <philipp.kober@stud.hawk.de>
2023-07-03 10:56:23 +0200

    viemodel classes change to ObservableObject Class. Fixed display Bug in MisterX Tickethistory View

--- InspectorGoe/Model/TicketsView.cs
+++ InspectorGoe/Model/TicketsView.cs
@@ -1,40 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
 
 namespace GameComponents.Model
 {
     /// <summary>
     /// Class to display the tickets 
     /// </summary>
-    public class TicketsView
+    public class TicketsView : ObservableObject
     {
         /// <summary>
         /// Ticket Imagepath
         /// </summary>
-        public string ImagePath { get; set; }
+        private string imagePath;
+        public string ImagePath { get => imagePath; set => SetProperty(ref imagePath, value); }
 
         /// <summary>
         /// Number of the Round
         /// </summary>
-        public int NumberRound { get; set; }
+        private int numberRound ;
+        public int NumberRound { get => numberRound; set => SetProperty(ref numberRound, value); }
 
         /// <summary>
         /// Borderthickness for the border of the ticket if Misterx Position is discovered
         /// </summary>
-        public int BorderThickness { get; set; }
+
+        private int borderThickness;
+        public int BorderThickness { get => borderThickness; set => SetProperty(ref borderThickness, value); }
 
         /// <summary>
         /// Color for the Frame behind the Roundnumber (only if ther is no ticket)
         /// </summary>
-        public Color NumberFrameColor { get; set; }
+        private Color numberFrameColor;
+        public Color NumberFrameColor { get => numberFrameColor; set => SetProperty(ref numberFrameColor, value); }
 
         /// <summary>
         /// Color of the Roundnumber
         /// </summary>
-        public Color NumberColor { get; set; }
+        private Color numberColor;
+        public Color NumberColor { get => numberColor; set => SetProperty (ref numberColor, value); }
 
     }
 }
