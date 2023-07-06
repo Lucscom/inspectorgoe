6157dceba4adca046a21bd1cf631063f7eebd721
philippkober <philipp.kober@stud.hawk.de>
2023-07-06 16:14:06 +0200

    Fixed Avatar Path in Lobby

--- InspectorGoeServer/Controllers/PlayerController.cs
+++ InspectorGoeServer/Controllers/PlayerController.cs
@@ -236,6 +236,7 @@ namespace InspectorGoeServer.Controllers
 
             var gamePlayer = (_gameController.GameState.AllPlayers.Where(p => p.UserName == currentUser.UserName)).First();
             gamePlayer.AvatarImagePath = path.token;
+            await updateGameComponents(_gameController.GameState);
             return Ok();
         }
 
